410c9fc38ec5208a33b4fecf5b64bb0d596987f8
PL <119684139+LanPenn@users.noreply.github.com>
2026-04-29 17:59:47 +0800

    Update Collateral.sol

--- src/Collateral.sol
+++ src/Collateral.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 import  "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Collateral is ERC20{
       constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
